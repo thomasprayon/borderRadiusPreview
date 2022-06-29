96bf2ca30fbbc7a9c7cc0d1fdb326ef9c160eeb7
thomasprayon <thomasprayongowland@gmail.com>
2022-06-29 15:32:12 +0200

    finished the project with the styling too

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const previewBtn = document.querySelector('.btn-preview');
 const copyBtn = document.querySelector('.btn-copy');
 const resetBtn = document.querySelector('.btn-reset');
 const makeCircle = document.querySelector('.btn-circle');
+const errorMessage = document.querySelector('.copy-message');
 let inputTL = document.querySelector('.input-top-left');
 let inputTR = document.querySelector('.input-top-right');
 let inputBL = document.querySelector('.input-bottom-left');
 let inputBR = document.querySelector('.input-bottom-right');
+let previewText = document.getElementById('infoPreviewer');
+let containerBox = document.querySelector('.container');
+let copyText = document.getElementById('infoPreviewer');
 
 // Check that the user gives valid values and until 100
 const checkInputUser = (bottomLeft, bottomRight, topLeft, topRight) => {
@@ -16,27 +20,20 @@ const checkInputUser = (bottomLeft, bottomRight, topLeft, topRight) => {
     topLeft = inputTL.value;
     topRight = inputTR.value;
 
-    //add error message
-    Number(topLeft) > 100 ||
-    Number(topRight) > 100 ||
-    Number(bottomLeft) > 100 ||
-    Number(bottomRight) > 100
-        ? alert('Is a lot to take, try less than 100')
-        : previewBorders(bottomLeft, bottomRight, topLeft, topRight);
-
+    Number(topLeft) > 120 ||
     Number(topLeft) < 0 ||
+    Number(topRight) > 120 ||
     Number(topRight) < 0 ||
+    Number(bottomLeft) > 120 ||
     Number(bottomLeft) < 0 ||
+    Number(bottomRight) > 120 ||
     Number(bottomRight) < 0
-        ? alert('less than 0 is not possible, try again')
+        ? (previewText.value = 'Please try again, invalid input!')
         : previewBorders(bottomLeft, bottomRight, topLeft, topRight);
 };
 
 // Edit borders by submitting values
 const previewBorders = (bottomLeft, bottomRight, topLeft, topRight) => {
-    let containerBox = document.querySelector('.container');
-    let previewText = document.getElementById('infoPreviewer');
-
     if (topLeft && !topRight && !bottomRight && !bottomLeft) {
         containerBox.style.borderTopLeftRadius = `${topLeft}px`;
         previewText.value = `border-left-top-radius: ${topLeft}px`;
@@ -71,7 +68,7 @@ const previewBorders = (bottomLeft, bottomRight, topLeft, topRight) => {
         containerBox.style.borderTopRightRadius = `${topRight}px`;
         containerBox.style.borderBottomRightRadius = `${bottomRight}px`;
         containerBox.style.borderBottomLeftRadius = `${bottomLeft}px`;
-        previewText.value = `border-top-right-radius: ${topRight} \nborder-bottom-right-radius: ${bottomRight}px \nborder-bottom-left-radius: ${bottomLeft}px `;
+        previewText.value = `border-top-right-radius: ${topRight}px \nborder-bottom-right-radius: ${bottomRight}px \nborder-bottom-left-radius: ${bottomLeft}px`;
     } else if (topLeft && !topRight && bottomRight && bottomLeft) {
         containerBox.style.borderTopLeftRadius = `${topLeft}px`;
         containerBox.style.borderBottomRightRadius = `${bottomRight}px`;
@@ -86,7 +83,7 @@ const previewBorders = (bottomLeft, bottomRight, topLeft, topRight) => {
         containerBox.style.borderTopLeftRadius = `${topLeft}px`;
         containerBox.style.borderTopRightRadius = `${topRight}px`;
         containerBox.style.borderBottomRightRadius = `${bottomRight}px`;
-        previewText.value = `border-top-left-radius: ${topLeft} \nborder-top-right-radius: ${topRight}px \nborder-bottom-right-radius: ${bottomRight}px`;
+        previewText.value = `border-top-left-radius: ${topLeft}px \nborder-top-right-radius: ${topRight}px \nborder-bottom-right-radius: ${bottomRight}px`;
     } else if (!topLeft && topRight && bottomRight && !bottomLeft) {
         containerBox.style.borderTopRightRadius = `${topRight}px`;
         containerBox.style.borderBottomRightRadius = `${bottomRight}px`;
@@ -94,26 +91,26 @@ const previewBorders = (bottomLeft, bottomRight, topLeft, topRight) => {
     } else if (topLeft && !topRight && bottomRight && !bottomLeft) {
         containerBox.style.borderTopLeftRadius = `${topLeft}px`;
         containerBox.style.borderBottomRightRadius = `${bottomRight}px`;
-        previewText.value = `border-top-left-radius:${topLeft}px \nborder-radius-bottom-right:${bottomRight}`;
+        previewText.value = `border-top-left-radius:${topLeft}px \nborder-radius-bottom-right:${bottomRight}px`;
     } else if (topLeft && topRight && !bottomRight && !bottomLeft) {
         containerBox.style.borderTopLeftRadius = `${topLeft}px`;
         containerBox.style.borderTopRightRadius = `${topRight}px`;
-        previewText.value = `border-top-left-radius: ${topLeft}px \nborder-radius-top-radius: ${topRight}`;
+        previewText.value = `border-top-left-radius: ${topLeft}px \nborder-radius-top-radius: ${topRight}px`;
     } else if (topLeft && !topRight && !bottomRight && bottomLeft) {
         containerBox.style.borderTopLeftRadius = `${topLeft}px`;
         containerBox.style.borderBottomLeftRadius = `${bottomLeft}px`;
         previewText.value = `border-top-left-radius: ${topLeft}px \nborder-radius-bottom-left: ${bottomLeft}px`;
     } else {
-        previewText.value = 'Something went wrong!';
+        previewText.value = 'Please enter values to preview!';
     }
 };
 
 // Copy text to clipboard
 const copyToClipboard = () => {
-    let copyText = document.getElementById('infoPreviewer');
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value);
+    previewText.value = 'Copied!';
 };
 
 //Buttons Functinality
@@ -123,14 +120,13 @@ previewBtn.addEventListener('click', () => {
 
 //Reset button
 resetBtn.addEventListener('click', () => {
-    let containerBox = document.querySelector('.container');
-    let previewText = document.getElementById('infoPreviewer');
     inputBL.value = '';
     inputBR.value = '';
     inputTL.value = '';
     inputTR.value = '';
     containerBox.style.borderRadius = '0px';
     previewText.value = '';
+    errorMessage.classList.add('no-show');
 });
 
 //CopyButton
@@ -138,9 +134,6 @@ copyBtn.addEventListener('click', copyToClipboard);
 
 //Make a circle
 makeCircle.addEventListener('click', () => {
-    console.log('makeCircle');
-    let containerBox = document.querySelector('.container');
-    let previewText = document.getElementById('infoPreviewer');
     containerBox.style.borderRadius = `100%`;
     previewText.value = `border-radius: 100%`;
 });
